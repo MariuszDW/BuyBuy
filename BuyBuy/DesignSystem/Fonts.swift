fa8197f6bb30d28a523218f2b35228456ba7e88b
Mariusz Wlodarczyk <mdw@amiga.pl>
2025-06-09 01:36:52 +0200

    Added monospaced fonts.

--- BuyBuy/DesignSystem/Fonts.swift
+++ BuyBuy/DesignSystem/Fonts.swift
@@ -31,4 +31,16 @@ extension Font {
     static func titleDynamic(style: Font.TextStyle) -> Font {
         .system(style, design: .rounded).weight(.semibold)
     }
+    
+    static func regularMonospaced(style: Font.TextStyle) -> Font {
+        .system(style, design: .monospaced)
+    }
+    
+    static func boldMonospaced(style: Font.TextStyle) -> Font {
+        .system(style, design: .monospaced).weight(.bold)
+    }
+    
+    static func titleMonospaced(style: Font.TextStyle) -> Font {
+        .system(style, design: .monospaced).weight(.semibold)
+    }
 }
