7a2626841f741c77ce7262c15ef1f43812709719
Mariusz Wlodarczyk <mdw@amiga.pl>
2025-06-17 21:05:00 +0200

    Change useful Date extension.

--- BuyBuy/Extensions/Date+Extensions.swift
+++ BuyBuy/Extensions/Date+Extensions.swift
@@ -10,11 +10,19 @@ import Foundation
 extension Date {
     func localizedString(dateStyle: DateFormatter.Style = .medium,
                          timeStyle: DateFormatter.Style = .none,
-                         locale: Locale = Locale.current) -> String {
+                         locale: Locale = Locale.current,
+                         timeZone: TimeZone = .current) -> String {
         let formatter = DateFormatter()
         formatter.dateStyle = dateStyle
         formatter.timeStyle = timeStyle
         formatter.locale = locale
+        formatter.timeZone = timeZone
+        return formatter.string(from: self)
+    }
+    
+    func iso8601UTCString() -> String {
+        let formatter = ISO8601DateFormatter()
+        formatter.timeZone = TimeZone(identifier: "UTC")
         return formatter.string(from: self)
     }
 }
