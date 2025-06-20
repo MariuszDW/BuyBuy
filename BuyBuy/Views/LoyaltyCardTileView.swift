3b0fc5bfe5b80b58547bae0ba5f2b74011668f61
Mariusz Wlodarczyk <mdw@amiga.pl>
2025-06-20 20:01:05 +0200

    Modify structure of loyalty card tile.

--- BuyBuy/Views/LoyaltyCardTileView.swift
+++ BuyBuy/Views/LoyaltyCardTileView.swift
@@ -26,31 +26,18 @@ struct LoyaltyCardTileView: View {
     
     var body: some View {
         VStack(alignment: .center, spacing: 6) {
-            ZStack {
-                if let image = thumbnail {
-                    Image(uiImage: image)
-                        .resizable()
-                        .scaledToFill()
-                        .clipped()
-                } else {
-                    Image(systemName: "creditcard.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: tileWidth * 0.65, height: tileWidth * 0.65)
-                        .foregroundColor(.bb.text.quaternary)
+            imageView
+                .frame(width: tileWidth, height: tileWidth)
+                .background(Color.bb.background2)
+                .cornerRadius(Self.cornderRadius)
+                .contentShape(Rectangle())
+                .clipShape(RoundedRectangle(cornerRadius: Self.cornderRadius))
+                .shadow(color: Color.black.opacity(0.4), radius: 5, x: 0, y: 2)
+                .overlay {
+                    RoundedRectangle(cornerRadius: Self.cornderRadius)
+                        .stroke(strokeLineColor, lineWidth: strokeLineWidth)
                 }
-            }
-            .frame(width: tileWidth, height: tileWidth)
-            .background(Color.bb.background2)
-            .cornerRadius(Self.cornderRadius)
-            .contentShape(Rectangle())
-            .clipShape(RoundedRectangle(cornerRadius: Self.cornderRadius))
-            .shadow(color: Color.black.opacity(0.4), radius: 5, x: 0, y: 2)
-            .overlay {
-                RoundedRectangle(cornerRadius: Self.cornderRadius)
-                    .stroke(strokeLineColor, lineWidth: strokeLineWidth)
-            }
-
+            
             Text(name)
                 .font(.regularDynamic(style: .headline))
                 .foregroundColor(.bb.text.primary)
@@ -59,5 +46,21 @@ struct LoyaltyCardTileView: View {
                 .frame(width: tileWidth)
         }
         .frame(width: tileWidth, alignment: .top)
+    }
+    
+    @ViewBuilder
+    private var imageView: some View {
+        if let image = thumbnail {
+            Image(uiImage: image)
+                .resizable()
+                .scaledToFill()
+                .clipped()
+        } else {
+            Image(systemName: "creditcard.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: tileWidth * 0.65, height: tileWidth * 0.65)
+                .foregroundColor(.bb.text.quaternary)
+        }
     }
 }
