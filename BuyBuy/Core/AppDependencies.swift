150b92c804b8f5c374864bdd7b7fd91b9546a22d
Mariusz Wlodarczyk <mdw@amiga.pl>
2025-06-23 16:40:29 +0200

    Change wrong declaration od data repository.

--- BuyBuy/Core/AppDependencies.swift
+++ BuyBuy/Core/AppDependencies.swift
@@ -12,7 +12,7 @@ final class AppDependencies: ObservableObject {
     let imageStorage: ImageStorage
     let fileStorage: FileStorage
     let coreDataStack: CoreDataStack
-    let repository: DataRepositoryProtocol
+    let repository: DataRepository
     let dataManager: DataManager
     
     @MainActor
