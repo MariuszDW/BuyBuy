4b1c41099fdb7d8ba94d382595edccaa6b13ae7a
Mariusz Wlodarczyk <mdw@amiga.pl>
2025-10-08 16:20:50 +0200

    Add animation of bottom row.

--- BuyBuy/Views/ShoppingListView.swift
+++ BuyBuy/Views/ShoppingListView.swift
@@ -70,6 +70,7 @@ struct ShoppingListView: View {
                     )
                     .padding(.horizontal)
                     .padding(.bottom, 4)
+                    .animation(.easeInOut, value: selectedItemStatus)
                 }
             } else {
                 EmptyView()
