397f4d0398c8e6df9609b42dd28b412f370f046b
Mariusz Wlodarczyk <mariusz.d.wlodarczyk@gmail.com>
2025-11-27 23:43:59 +0100

    Implement Color extenstion for aplly HSB to a color.

--- BuyBuy/Extensions/Color+Extensions.swift
+++ BuyBuy/Extensions/Color+Extensions.swift
@@ -24,4 +24,26 @@ extension Color {
 
         return String(format: "#%02X%02X%02X", r, g, b)
     }
+    
+    func hsb(hue: Double = 0, saturation: Double = 0, brightness: Double = 0) -> Color {
+        let ui = UIColor(self)
+        
+        var h: CGFloat = 0
+        var s: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
+        
+        ui.getHue(&h, saturation: &s, brightness: &b, alpha: &a)
+        
+        let newH = CGFloat((Double(h) + hue).truncatingRemainder(dividingBy: 1.0))
+        let newS = CGFloat(min(max(Double(s) + saturation, 0), 1))
+        let newB = CGFloat(min(max(Double(b) + brightness, 0), 1))
+        
+        return Color(
+            hue: Double(newH),
+            saturation: Double(newS),
+            brightness: Double(newB),
+            opacity: Double(a)
+        )
+    }
 }
